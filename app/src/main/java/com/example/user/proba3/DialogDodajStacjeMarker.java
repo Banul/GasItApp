b2e15487e8c01f48540d4f7730532f499d42cafd
Marcin <marcin919@op.pl>
2017-05-24 15:00:13 +0200

    zmiana dialogu dodawania stacji i lista stacji

--- app/src/main/java/com/example/user/proba3/DialogDodajStacjeMarker.java
+++ app/src/main/java/com/example/user/proba3/DialogDodajStacjeMarker.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
@@ -37,13 +38,13 @@ import java.util.ArrayList;
  * Created by User on 2017-04-24.
  */
 
-public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterView.OnItemSelectedListener,RequestCallback<String> {
+public class DialogDodajStacjeMarker extends DialogFragment implements AdapterView.OnItemSelectedListener, RequestCallback<String> {
 
     private GoogleMap mapa;
     private LatLng polozenie;
     private Spinner spinner;
     private Spinner spinner2;
-    private Activity activity ;
+    private Activity activity;
     private ImageButton przyciskMinus;
     private ImageButton przyciskPlus;
     private EditText tekst;
@@ -51,22 +52,22 @@ public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterV
     CustomAdapterStacje adapterStacje;
     public ArrayList<ItemData> CustomListViewValuesArr = new ArrayList<ItemData>();
     public ArrayList<ItemData> ListaNaStacje = new ArrayList<ItemData>();
-    public ArrayList ListaNaStacjeDoDodaniaNaMape = new ArrayList<GasStation>();
     private boolean CzyMoznaDodacZnacznikNaMape = false;
+    private Button dodajPaliwo;
+    ArrayList<Gas> gases;
+
     String url = "https://script.google.com/macros/s/AKfycbwi_fjw8oLX5gYWuPmukORIFkV4S-hzJRqBlIFngtLCq7uE5j4/exec";
 
 
-    public DialogDodajStacjeMarker()
-    {
+    public DialogDodajStacjeMarker() {
 
     }
 
-    public DialogDodajStacjeMarker(LatLng ltlng, ArrayList listStacji, GoogleMap mapka)
+    public DialogDodajStacjeMarker(LatLng ltlng, GoogleMap mapka)
 
     {
         this.mapa = mapka;
         this.polozenie = ltlng;
-        this.ListaNaStacjeDoDodaniaNaMape = listStacji;
     }
 
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -78,8 +79,10 @@ public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterV
         spinner2 = (Spinner) view.findViewById(R.id.spinner2); // dla paliw
         spinner = (Spinner) view.findViewById(R.id.spinner);// dla stacji
         przyciskMinus = (ImageButton) view.findViewById(R.id.przyciskMinus);
-        przyciskPlus= (ImageButton) view.findViewById(R.id.przyciskPlus);
+        przyciskPlus = (ImageButton) view.findViewById(R.id.przyciskPlus);
         tekst = (EditText) view.findViewById(R.id.tekst);
+        dodajPaliwo = (Button) view.findViewById(R.id.dodajPaliwo);
+        gases = new ArrayList<Gas>();
 
 
         przyciskMinus.setOnClickListener(new View.OnClickListener() {
@@ -89,16 +92,16 @@ public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterV
 
 
                 String text = String.valueOf(tekst.getText());
-                Log.d("mesydz",text);
+                Log.d("mesydz", text);
                 Double wartD = Double.parseDouble(text);
                 String wartoscS;
                 wartoscS = df.format(wartD);
-                wartoscS=wartoscS.replace(",",".");
-                Log.d("mesydz1",wartoscS);
+                wartoscS = wartoscS.replace(",", ".");
+                Log.d("mesydz1", wartoscS);
                 Double wartoscD = Double.parseDouble(wartoscS);
                 String wartoscDS = String.valueOf(wartoscD);
-                Log.d("mesydz2",wartoscDS);
-                Double mniejszeWartD = wartoscD-0.01;
+                Log.d("mesydz2", wartoscDS);
+                Double mniejszeWartD = wartoscD - 0.01;
 
 
                 String toWpisz = df.format(mniejszeWartD);
@@ -115,16 +118,16 @@ public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterV
 
 
                 String text = String.valueOf(tekst.getText());
-                Log.d("mesydz",text);
+                Log.d("mesydz", text);
                 Double wartD = Double.parseDouble(text);
                 String wartoscS;
                 wartoscS = df.format(wartD);
-                wartoscS=wartoscS.replace(",",".");
-                Log.d("mesydz1",wartoscS);
+                wartoscS = wartoscS.replace(",", ".");
+                Log.d("mesydz1", wartoscS);
                 Double wartoscD = Double.parseDouble(wartoscS);
                 String wartoscDS = String.valueOf(wartoscD);
-                Log.d("mesydz2",wartoscDS);
-                Double mniejszeWartD = wartoscD+0.01;
+                Log.d("mesydz2", wartoscDS);
+                Double mniejszeWartD = wartoscD + 0.01;
 
 
                 String toWpisz = df.format(mniejszeWartD);
@@ -138,69 +141,66 @@ public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterV
         setListDataPaliwa();
         Resources res = getResources();
 
-        adapter = new CustomAdapter(activity, R.layout.spinner_rows, CustomListViewValuesArr,res);
-        adapterStacje = new CustomAdapterStacje(activity, R.layout.spinner_rows, ListaNaStacje,res);
+        adapter = new CustomAdapter(activity, R.layout.spinner_rows, CustomListViewValuesArr, res);
+        adapterStacje = new CustomAdapterStacje(activity, R.layout.spinner_rows, ListaNaStacje, res);
 
 
         spinner.setAdapter(adapterStacje);
         spinner2.setAdapter(adapter);
+
+
+        dodajPaliwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (polozenie != null) {
+                    Marker markerek = mapa.addMarker(new MarkerOptions().position(polozenie));
+                }
+
+                ItemData wybranePaliwo = (ItemData) spinner2.getSelectedItem();
+                String nazwaPaliwa = wybranePaliwo.getText();
+                String cena = String.valueOf(tekst.getText());
+                Double cenaD = Double.valueOf(cena);
+
+                Gas gaz = new Gas(nazwaPaliwa, cenaD);
+                if (polozenie != null) {
+
+                    gases.add(gaz);
+                }
+            }
+        });
 
         builder.setView(view)
                 // Add action buttons
                 .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
-                      //  ArrayList<String> lista = new ArrayList<String>();
-                       // Log.d("okok","okok");
-                        if (polozenie!=null) {
-                            Marker markerek = mapa.addMarker(new MarkerOptions().position(polozenie));
-                            Log.d("dodaje cholerny marker","aaa");
-                            ListaNaStacjeDoDodaniaNaMape.add(markerek);
-                        }
                         ItemData wybranaStacja = (ItemData) spinner.getSelectedItem();
                         String nazwaStacji = wybranaStacja.getText();
-                        ItemData wybranePaliwo = (ItemData) spinner2.getSelectedItem();
-                        String nazwaPaliwa = wybranePaliwo.getText();
-                        String cena = String.valueOf(tekst.getText());
-                        Double cenaD = Double.valueOf(cena);
-
-                       Gas gaz = new Gas(nazwaPaliwa,cenaD);
-                        if (polozenie!=null) {
-                            ArrayList<Gas> gases = new ArrayList<Gas>();
-                        gases.add(gaz);
 
 
+                        GasStation stacjaBenz = new GasStation(nazwaStacji, polozenie.latitude, polozenie.longitude, gases);
 
-                            GasStation stacjaBenz = new GasStation(nazwaStacji,polozenie.latitude,polozenie.longitude, gases);
+                        String stacjaDoWyslania;
 
-                            String stacjaDoWyslania;
+                        try {
+                            stacjaDoWyslania = stacjaBenz.toJSON().toString();
+                            UploadRequestTask uploadRequestTask = new UploadRequestTask(new RequestCallback<String>() {
+                                @Override
+                                public void updateFromResponse(String response) {
+                                    String kodOdpowiedzi = response;
 
-                            try {
-                                stacjaDoWyslania = stacjaBenz.toJSON().toString();
-                                UploadRequestTask uploadRequestTask = new UploadRequestTask(new RequestCallback<String>() {
-                                    @Override
-                                    public void updateFromResponse(String response) {
-                                        String kodOdpowiedzi = response;
+                                }
+                            });
 
-                                    }
-                                });
-
-                                uploadRequestTask.execute(url,"POST","addStation",stacjaDoWyslania);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-
-
-
-//                        Log.d("banul12",lista.get(0));
-//                        Log.d("banul",lista.get(1));
-//                        Log.d("banul",lista.get(2));
-
-                        //wyślij tę listę do API
+                            uploadRequestTask.execute(url, "POST", "addStation", stacjaDoWyslania);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
                     }
-                }})
+
+
+                })
                 .setNegativeButton("Wyjdz", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
@@ -220,53 +220,53 @@ public class  DialogDodajStacjeMarker extends DialogFragment implements AdapterV
 
     }
 
-    public boolean ZwrocCzyMoznaPostawicZnacznikNaMape()
-    {
+    public boolean ZwrocCzyMoznaPostawicZnacznikNaMape() {
         return this.CzyMoznaDodacZnacznikNaMape;
     }
 
-    public void UstawCzyMoznaPostawicZnacznikNaMape(boolean b)
-    {
+    public void UstawCzyMoznaPostawicZnacznikNaMape(boolean b) {
         this.CzyMoznaDodacZnacznikNaMape = b;
     }
-    public void setListData() {
-            final ItemData sched = new ItemData();
-            final ItemData sched1 = new ItemData();
-            final ItemData sched2= new ItemData();
-            final ItemData sched3 = new ItemData();
-            final ItemData sched4 = new ItemData();
 
-            /******* Firstly take data in model object ******/
+    public void setListData() {
+        final ItemData sched = new ItemData();
+        final ItemData sched1 = new ItemData();
+        final ItemData sched2 = new ItemData();
+        final ItemData sched3 = new ItemData();
+        final ItemData sched4 = new ItemData();
+
+        /******* Firstly take data in model object ******/
 
         sched.setText("Please select company ");
         CustomListViewValuesArr.add(sched);
-            sched1.setText("PB95" );
-            CustomListViewValuesArr.add(sched1);
-        sched2.setText("PB98" );
+        sched1.setText("PB95");
+        CustomListViewValuesArr.add(sched1);
+        sched2.setText("PB98");
         CustomListViewValuesArr.add(sched2);
-            sched3.setText("ON ");
-            CustomListViewValuesArr.add(sched3);
-            sched4.setText("LPG ");
+        sched3.setText("ON ");
+        CustomListViewValuesArr.add(sched3);
+        sched4.setText("LPG ");
         CustomListViewValuesArr.add(sched4);
     }
-    public void setListDataPaliwa() {
-            final ItemData sched = new ItemData();
-            final ItemData sched1 = new ItemData();
-            final ItemData sched2= new ItemData();
-            final ItemData sched3 = new ItemData();
-            final ItemData sched4 = new ItemData();
 
-            /******* Firstly take data in model object ******/
+    public void setListDataPaliwa() {
+        final ItemData sched = new ItemData();
+        final ItemData sched1 = new ItemData();
+        final ItemData sched2 = new ItemData();
+        final ItemData sched3 = new ItemData();
+        final ItemData sched4 = new ItemData();
+
+        /******* Firstly take data in model object ******/
 
         sched.setText("Prosze podaj nazwe paliwa");
         ListaNaStacje.add(sched);
-            sched1.setText("Shell" );
-            ListaNaStacje.add(sched1);
-        sched2.setText("Orlen" );
+        sched1.setText("Shell");
+        ListaNaStacje.add(sched1);
+        sched2.setText("Orlen");
         ListaNaStacje.add(sched2);
-            sched3.setText("Statoil");
-            ListaNaStacje.add(sched3);
-            sched4.setText("Lukoil ");
+        sched3.setText("Statoil");
+        ListaNaStacje.add(sched3);
+        sched4.setText("Lukoil ");
         ListaNaStacje.add(sched4);
     }
 
